e2cbae9df232dd7c1ada2c799e87fb7d804e6cb3
Dolly Desir <dolly.desir@gmail.com>
2020-05-07 01:46:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,8 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
 
-array.each do
-  p "Hello #{array} !"
+greet_characters.each do |greet_characters|
+  p "Hello #{greet_characters} !"
   
 end
 
