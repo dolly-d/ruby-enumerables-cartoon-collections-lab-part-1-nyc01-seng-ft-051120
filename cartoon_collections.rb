f66440ecfb048bbb3b9db3f9b105d58c2636b497
Dolly Desir <dolly.desir@gmail.com>
2020-05-07 03:19:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index do |dwarves, index|
-    output << #{index +1}. #{dwarves}
-    puts output
+    dwarves << #{index +1}. #{dwarves}
+    puts dwarves
     end
 end
