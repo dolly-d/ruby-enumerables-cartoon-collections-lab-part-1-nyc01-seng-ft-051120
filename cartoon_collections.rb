8102a3498f6eccb0d8cb23c87af5910a423e4df7
Dolly Desir <dolly.desir@gmail.com>
2020-05-07 01:48:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
 
-  array.each do |greet_characters|
+  array.each do |array|
   p "Hello #{greet_characters} !"
   end
   
